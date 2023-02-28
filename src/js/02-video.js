00f2631a2c05b125d37abe79cc4df52587fcb8d8
Khashyrova <121459042+Khashyrova@users.noreply.github.com>
2023-02-28 20:44:26 +0100

    adds

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,11 +7,16 @@ const player = new VimeoPlayer(iframe);
 const CURRENT_TIME_KEY =  'videoplayer-current-time';
 
 const onPlay = function(data) {
-   const time = data.seconds
+   const time = data.seconds.toFixed(2)
    console.log(time)
    localStorage.setItem(CURRENT_TIME_KEY, time);
 };
 
+if (localStorage.getItem(CURRENT_TIME_KEY))
+   player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)));
+
 player.on('timeupdate', throttle(onPlay, 1000));
 
-player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)));
+
+
+
