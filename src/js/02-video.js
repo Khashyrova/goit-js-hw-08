4225d92c0bc44856df4663d0f7b91d03a8c530bb
Khashyrova <121459042+Khashyrova@users.noreply.github.com>
2023-02-28 20:55:39 +0100

    adds!

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,7 +4,8 @@ import throttle from "lodash.throttle";
 const iframe = document.querySelector('iframe');
 const player = new VimeoPlayer(iframe);
 
-const CURRENT_TIME_KEY =  'videoplayer-current-time';
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+const getItem = localStorage.getItem(CURRENT_TIME_KEY);
 
 const onPlay = function(data) {
    const time = data.seconds.toFixed(2)
@@ -12,8 +13,8 @@ const onPlay = function(data) {
    localStorage.setItem(CURRENT_TIME_KEY, time);
 };
 
-if (localStorage.getItem(CURRENT_TIME_KEY))
-   player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)));
+if (getItem)
+   player.setCurrentTime(JSON.parse(getItem));
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
